51884df69f998d065e2a6369a45ccb3953bcc355
JeongaHan <ab012121@naver.com>
2018-11-22 02:13:07 -0800

    changed the congraturation messages

--- baseball.c
+++ baseball.c
@@ -42,14 +42,14 @@ void main()
 
      if((player[0] == player[1]) || (player[1] == player[2]) || (player[0] == player[2]))
      {
-       printf("no duplicated number! One more\n");
+       printf("no duplicated number! One more\n\n");
        //cnt--;
        continue;
      }
 
      if(player[0] == 0 || player[1] == 0 || player[2] == 0)
      {
-       printf("Do not enter zero\n");
+       printf("Do not enter zero\n\n");
        //cnt--;
        continue;
      }
@@ -69,8 +69,25 @@ void main()
 
      if(strike == 3)
      {
-       printf("Congratulation!!!!!Wow!\n");
-       break;
+       printf("**********Congratulation!!!!!Wow!***********\n\n");
+       int i,j;
+       for( i = 4 ; i < 10; i +=2 )
+       {
+	  for( j = 0; j <= (10 - i); j++ ){printf(" ");}
+          for( j = 0; j <= i * 2; j++ ){printf("%c",176);}
+          for( j = 0; j <= (10 - i) * 2; j++ ){printf(" ");}
+	  for( j = 0; j <= i * 2; j++ ){printf("%c",176);}
+	  printf("\n");
+       }
+       for( i = 20; i >= 0; i -= 2 )
+       {
+          for( j = 0;  j <= 20 - i; j++ ){ printf(" ");} 
+          for( j = 0; j <= i * 2; j++ ){printf("%c",176);}
+          printf("\n");
+       }
+       printf("**********Congratulation!!!!!Wow!***********\n\n");
+ 
+        break;
      }
      printf("%d strike %d ball \n\n", strike, ball);
 
