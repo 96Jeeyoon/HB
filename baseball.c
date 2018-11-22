cdfc6e0a4087ba93cfe51dbe45a870b4e3b25d35
JeongaHan <ab012121@naver.com>
2018-11-22 02:18:36 -0800

    change ascii msg to *

--- baseball.c
+++ baseball.c
@@ -74,15 +74,15 @@ void main()
        for( i = 4 ; i < 10; i +=2 )
        {
 	  for( j = 0; j <= (10 - i); j++ ){printf(" ");}
-          for( j = 0; j <= i * 2; j++ ){printf("%c",176);}
+          for( j = 0; j <= i * 2; j++ ){printf("*");}
           for( j = 0; j <= (10 - i) * 2; j++ ){printf(" ");}
-	  for( j = 0; j <= i * 2; j++ ){printf("%c",176);}
+	  for( j = 0; j <= i * 2; j++ ){printf("*");}
 	  printf("\n");
        }
        for( i = 20; i >= 0; i -= 2 )
        {
           for( j = 0;  j <= 20 - i; j++ ){ printf(" ");} 
-          for( j = 0; j <= i * 2; j++ ){printf("%c",176);}
+          for( j = 0; j <= i * 2; j++ ){printf("*");}
           printf("\n");
        }
        printf("**********Congratulation!!!!!Wow!***********\n\n");
