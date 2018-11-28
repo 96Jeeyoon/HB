c63c114fa7890eda5547d9777e45db1e48a6903d
96Jeeyoon <baek7922@naver.com>
2018-11-27 23:56:28 -0800

    add baseball_hard in main

--- source/main.c
+++ source/main.c
@@ -1,8 +1,25 @@
 #include "baseball.h"
+#include "basebll_hard.h"
 #include <stdio.h>
 
 int main(void)
 {
-	baseball();
+	int mode_num;
+	printf("1. Baseball game_easy mode.\n");
+	printf("2. Baseball game_hard mode.\n");
+	
+	scanf("%d",mode_num);
+
+	if(mode_num == 1) {
+		baseball();
+	}
+
+	else if(mode_num == 2){
+		baseball_hard();
+	}
+
+	else {
+		printf("only enter 1 or 2");	
+	}	
 	return 0;
 }
