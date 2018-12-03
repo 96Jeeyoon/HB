cc3d2ca0a30681b3c2de1d392f0e4a51ccfa4e68
96Jeeyoon <baek7922@naver.com>
2018-12-03 06:34:15 -0800

    edit main

--- source/main.c
+++ source/main.c
@@ -8,7 +8,8 @@ int main(void)
 	int mode_num;
 	printf("1. Baseball game_easy mode.\n");
 	printf("2. Baseball game_hard mode.\n");
-	
+	printf("3. Bingo game.\n");
+
 	scanf("%d",&mode_num);
 
 	if(mode_num == 1) {
@@ -19,8 +20,12 @@ int main(void)
 		baseball_hard();
 	}
 
+	else if(mode_num ==3) {
+		//bingo();
+	}
+
 	else {
-		printf("only enter 1 or 2");	
-	}	
+		printf("only enter 1 or 2");
+	}
 	return 0;
 }
