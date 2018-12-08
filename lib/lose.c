5df88dbfbfcc23275c74b665c503b601a785c3ef
JeongaHan <ab012121@naver.com>
2018-12-08 06:33:26 -0800

    add lose.c to lib directory

---
+++ lib/lose.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void lose()
+{
+	printf("\n\n\n**********YOU LOSE...******YOU LOSE..***********\n\n\n");
+
+	printf("          YOULOSEYOULOSEY\n");               
+	printf("       OULOSEYOULOSEYOULOSEY            !\n");
+	printf("    OULO   SEYOULOSEYOULOSEYO          HA\n");
+	printf("  YOULOS   EYOULOSEYOULOSEYOULO       YOU\n");
+	printf("SEYOULOSEYOULOSEYOULOSEYOULOSEYOU    LOSE\n");
+	printf("  LOSEYOULOSEYOULOSEYOULOSEYOULOSE  YOULO\n");
+	printf("      SEYOULOSEYOULOSEYOULOSEYOULOSEYOULO\n");
+	printf("  SEYOULOSEYOULOSEYOULOSEYOULOSEYO  ULOSE\n");
+	printf("YOULOSEYOULOSEYOULOSEYOULOSEYOULO    SEYO\n");
+	printf("  ULOSEYOULOSEYOULOSEYOULOSEYOU       LOS\n");
+	printf("    EYOULOSEYOULOSEYOULOSEYOUL         OS\n");
+	printf("       EYOULOSEYOULOSEYOULOS            E\n");
+	printf("         YOULOSEYOULOSEYOU              !\n");
+
+
+	printf("\n\n\n**********YOU LOSE..******YOU LOSE...***********\n\n\n\n");
+
+return 0;
+
+}
